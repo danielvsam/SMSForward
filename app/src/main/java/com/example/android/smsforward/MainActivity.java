75511b2504ef36815de33cdb35ccc921e8ffea62
danielvsam <34299804+danielvsam@users.noreply.github.com>
2017-12-16 01:19:21 +0530

    added telegram, cleanup http
    support for posting to and setup telegram bot credentials, stripped http to barebones and overall cleanup

--- app/src/main/java/com/example/android/smsforward/MainActivity.java
+++ app/src/main/java/com/example/android/smsforward/MainActivity.java
@@ -1,45 +1,37 @@
 package com.example.android.smsforward;
 
 import android.Manifest;
-import android.content.BroadcastReceiver;
-import android.content.Context;
-import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.os.AsyncTask;
 import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.telephony.SmsManager;
-import android.telephony.SmsMessage;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
-import java.io.BufferedOutputStream;
-import java.io.BufferedWriter;
 import java.io.IOException;
-import java.io.OutputStream;
-import java.io.OutputStreamWriter;
 import java.net.HttpURLConnection;
-import java.net.MalformedURLException;
-import java.net.SocketTimeoutException;
 import java.net.URL;
-
-import static android.R.attr.phoneNumber;
-import static android.icu.lang.UCharacter.GraphemeClusterBreak.L;
-import static android.webkit.ConsoleMessage.MessageLevel.LOG;
 
 public class MainActivity extends AppCompatActivity
         implements ActivityCompat.OnRequestPermissionsResultCallback{
 
     private static final int MY_PERMISSIONS_REQUEST_SEND_SMS = 0;
+    private static final int MY_PERMISSIONS_REQUEST_READ_SMS = 1;
+    private static final int MY_PERMISSIONS_REQUEST_RECEIVE_SMS = 2;
 
     private static MainActivity inst;
     private String mPhoneNumber;
     private String mForwardPhoneNumber = null;
     private String mMessage;
     private boolean forwardToSmsActive = false;
+    String mPostUrl = "https://api.telegram.org/bot{botToken}/sendMessage?chat_id={chatId}&text=";
+    String botToken;
+    String chatId;
 
     public static MainActivity instance() {
         return inst;
@@ -103,6 +95,12 @@ public class MainActivity extends AppCompatActivity
         ActivityCompat.requestPermissions(MainActivity.this,
                 new String[]{Manifest.permission.SEND_SMS},
                 MY_PERMISSIONS_REQUEST_SEND_SMS);
+        ActivityCompat.requestPermissions(MainActivity.this,
+                new String[]{Manifest.permission.READ_SMS},
+                MY_PERMISSIONS_REQUEST_READ_SMS);
+        ActivityCompat.requestPermissions(MainActivity.this,
+                new String[]{Manifest.permission.READ_SMS},
+                MY_PERMISSIONS_REQUEST_RECEIVE_SMS);
 
     }
 
@@ -123,25 +121,8 @@ public class MainActivity extends AppCompatActivity
                 }
             }
         }
-
-
-        String urlString = "https://api.telegram.org/"; // URL to call
-        String data = "bot489635845:AAGAy5DLIhfmv14QgLhQTAqgrYuvCAs5-MQ/sendMessage?chat_id=507477080&text=Hi+Daniel+test"; //data to post
-        OutputStream out = null;
-        try {
-            URL url = new URL(urlString);
-            HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
-            out = new BufferedOutputStream(urlConnection.getOutputStream());
-            BufferedWriter writer = new BufferedWriter (new OutputStreamWriter(out, "UTF-8"));
-            writer.write(data);
-            writer.flush();
-            writer.close();
-            out.close();
-            urlConnection.connect();
-        } catch (Exception e) {
-            System.out.println(e.getMessage());
-        }
-
+        PostTask task = new PostTask();
+        task.execute();
     }
 
 
@@ -153,7 +134,7 @@ public class MainActivity extends AppCompatActivity
 
     public void setForwardToSmsActive(View view) {
         Button forwardActive = (Button) findViewById(R.id.forwardActivate);
-        if(forwardToSmsActive == false && mForwardPhoneNumber != null) {
+        if(!forwardToSmsActive && mForwardPhoneNumber != null) {
             forwardToSmsActive = true;
             forwardActive.setText("Deactivate Forward To SMS");
         }
@@ -162,6 +143,47 @@ public class MainActivity extends AppCompatActivity
             forwardActive.setText("Activate Forward To SMS");
         }
     }
+
+    public void makePostRequest(View v) throws IOException {
+        Log.e("forward", "Sending to bot");
+        mMessage = "Test Message";
+        PostTask task = new PostTask();
+        task.execute();
+    }
+
+    public void setupTelegram(View v) {
+        EditText botTokenEditText = (EditText) findViewById(R.id.botTokenField);
+        EditText ChatIdEditText = (EditText) findViewById(R.id.chatIdField);
+        botToken = botTokenEditText.getText().toString();
+        chatId = ChatIdEditText.getText().toString();
+        mPostUrl = mPostUrl.replace("{botToken}","489635845:AAGAy5DLIhfmv14QgLhQTAqgrYuvCAs5-MQ");
+        mPostUrl = mPostUrl.replace("{chatId}","507477080");
+        Log.e("setupTelegram", mPostUrl);
+    }
+
+    private class PostTask extends AsyncTask<String, Void, Void> {
+
+        @Override
+        protected Void doInBackground(String... urls) {
+            String messageToSend = mMessage.replace(" ","+");
+            URL url;
+            HttpURLConnection urlConnection = null;
+            try {
+                url = new URL(mPostUrl + messageToSend);
+                urlConnection = (HttpURLConnection) url.openConnection();
+                urlConnection.setRequestMethod("POST");
+                urlConnection.setConnectTimeout(15000 /* milliseconds */);
+                urlConnection.connect();
+                urlConnection.getResponseCode();
+            } catch (IOException e) {
+                // TODO Handle the IOException
+            }
+            finally {
+                if (urlConnection != null) {
+                    urlConnection.disconnect();
+                }
+            }
+            return null;
+        }
+    }
 }
-
-
